71ea725d197e9925fb771664a2da0208250d4068
Rizwan Muzammal <129890916+Rizwanali324@users.noreply.github.com>
2024-08-03 18:07:23 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from langchain_community.llms import LlamaCpp
 from langchain.prompts import PromptTemplate
 from huggingface_hub import hf_hub_download
 
-# StreamHandler to intercept streaming output from the LLM.
 class StreamHandler:
     def __init__(self, container, initial_text=""):
         self.container = container
@@ -15,7 +14,7 @@ class StreamHandler:
 
 @st.cache_resource
 def create_chain(system_prompt):
-    (repo_id, model_file_name) = ("TheBloke/Mistral-7B-Instruct-v0.1-GGUF", "mistral-7b-instruct-v0.1.Q4_0.gguf")
+    repo_id, model_file_name = "TheBloke/Mistral-7B-Instruct-v0.1-GGUF", "mistral-7b-instruct-v0.1.Q4_0.gguf"
     model_path = hf_hub_download(repo_id=repo_id, filename=model_file_name, repo_type="model")
 
     llm = LlamaCpp(
@@ -34,18 +33,11 @@ def create_chain(system_prompt):
 
     return llm_chain
 
-# Set the webpage title
 st.set_page_config(page_title="Your own aiChat!")
-
-# Create a header element
 st.header("Your own aiChat!")
 
-# This sets the LLM's personality for each prompt.
-system_prompt = st.text_area(
-    label="System Prompt",
-    value="You are a helpful AI assistant who answers questions in short sentences.",
-    key="system_prompt"
-)
+# Set the system prompt here directly without displaying it
+system_prompt = "You are a helpful AI assistant who answers questions in short sentences."
 
 # Create LLM chain to use for our chatbot.
 llm_chain = create_chain(system_prompt)
@@ -61,7 +53,6 @@ for message in st.session_state.messages:
 
 # We take questions/instructions from the chat input to pass to the LLM
 if user_prompt := st.chat_input("Your message here", key="user_input"):
-
     # Add our input to the session state
     st.session_state.messages.append({"role": "user", "content": user_prompt})
 
